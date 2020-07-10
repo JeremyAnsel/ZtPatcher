0466c103e8bc8eafdf8f0578b6a9c0d09959ce39
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-07-10 16:41:02 +0200

    Add xwa exe sections

---
+++ Zt/ZtPatcher/ExeSection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZtPatcher
+{
+    class ExeSection
+    {
+        public ExeSection(string name, int tableSize, int entrySize, int offset)
+        {
+            this.Name = name;
+            this.TableSize = tableSize;
+            this.EntrySize = entrySize;
+            this.Offset = offset;
+        }
+
+        public string Name { get; }
+
+        public int TableSize { get; }
+
+        public int EntrySize { get; }
+
+        public int Offset { get; }
+    }
+}
--- Zt/ZtPatcher/PatchItemConverter.cs
+++ Zt/ZtPatcher/PatchItemConverter.cs
@@ -7,6 +7,15 @@ namespace ZtPatcher
 {
     class PatchItemConverter : BaseConverter, IValueConverter
     {
+        private static readonly List<ExeSection> exeSections = new List<ExeSection>
+        {
+            new ExeSection("Species", 232 + 1, 0x02, 0x005B0F70 - 0x00401400),
+            new ExeSection("Object", 557, 0x18, 0x005FB240 - 0x00401400),
+            new ExeSection("Craft", 265, 0x3DB, 0x005BB480 - 0x00401400),
+            new ExeSection("HullIcon", 72, 0x02, 0x005A9608 - 0x00401000),
+            new ExeSection("MapIcon", 222, 0x02, 0x005A9AD8 - 0x00401000),
+        };
+
         public PatchItemConverter()
         {
         }
@@ -15,12 +24,30 @@ namespace ZtPatcher
         {
             var item = (KeyValuePair<int, byte[]>)value;
 
-            return string.Format(CultureInfo.InvariantCulture, "{0:X8} : {1}", item.Key, BitConverter.ToString(item.Value));
+            return string.Format(CultureInfo.InvariantCulture, "{0:X8} {1} V={2}", item.Key, GetExeSectionString(item.Key), BitConverter.ToString(item.Value));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private static string GetExeSectionString(int offset)
+        {
+            foreach (ExeSection section in exeSections)
+            {
+                if (offset >= section.Offset && offset < (section.Offset + section.TableSize * section.EntrySize))
+                {
+                    int delta = offset - section.Offset;
+
+                    int entryIndex = delta / section.EntrySize;
+                    int entryOffset = delta % section.EntrySize;
+
+                    return string.Format(CultureInfo.InvariantCulture, "{0} I={1} O={2}", section.Name, entryIndex, entryOffset);
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
