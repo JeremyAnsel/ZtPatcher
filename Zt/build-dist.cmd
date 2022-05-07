843b216809a6b2a794621c83c38b8b50502f5a26
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-05-07 15:11:52 +0200

    Switch to VS  2022

--- Zt/build-dist.cmd
+++ Zt/build-dist.cmd
@@ -4,31 +4,31 @@ setlocal
 cd "%~dp0"
 
 For %%a in (
-"Zt\bin\Release\net40\*.dll"
+"Zt\bin\Release\net48\*.dll"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
 
 For %%a in (
-"ZtCreator\bin\Release\net40\*.dll"
-"ZtCreator\bin\Release\net40\*.exe"
-"ZtCreator\bin\Release\net40\*.config"
+"ZtCreator\bin\Release\net48\*.dll"
+"ZtCreator\bin\Release\net48\*.exe"
+"ZtCreator\bin\Release\net48\*.config"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
 
 For %%a in (
-"ZtPatcher\bin\Release\net40\*.dll"
-"ZtPatcher\bin\Release\net40\*.exe"
-"ZtPatcher\bin\Release\net40\*.config"
+"ZtPatcher\bin\Release\net48\*.dll"
+"ZtPatcher\bin\Release\net48\*.exe"
+"ZtPatcher\bin\Release\net48\*.config"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
 
 For %%a in (
-"ZtBlank\bin\Release\net40\*.dll"
-"ZtBlank\bin\Release\net40\*.exe"
-"ZtBlank\bin\Release\net40\*.config"
+"ZtBlank\bin\Release\net48\*.dll"
+"ZtBlank\bin\Release\net48\*.exe"
+"ZtBlank\bin\Release\net48\*.config"
 ) do (
 xcopy /s /d "%%~a" dist\
 )
